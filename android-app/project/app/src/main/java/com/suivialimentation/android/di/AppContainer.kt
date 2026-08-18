ceab1b3db6309f08eedd73135633908ca7736b16
gaboriaudn <155800237+gaboriaudn@users.noreply.github.com>
2026-08-18 19:38:43 +0200

    J1.8: expose photo analysis service

--- android-app/project/app/src/main/java/com/suivialimentation/android/di/AppContainer.kt
+++ android-app/project/app/src/main/java/com/suivialimentation/android/di/AppContainer.kt
@@ -6,6 +6,7 @@ import com.suivialimentation.android.auth.OAuthConfig
 import com.suivialimentation.android.auth.SecureTokenStore
 import com.suivialimentation.android.data.ha.HomeAssistantApi
 import com.suivialimentation.android.data.ha.HomeAssistantWebSocketClient
+import com.suivialimentation.android.data.photo.PhotoAnalysisService
 import com.suivialimentation.android.data.repository.DefaultNutritionRepository
 import com.suivialimentation.android.data.repository.NutritionRepository
 import com.suivialimentation.android.data.repository.OperationStore
@@ -34,6 +35,8 @@ class AppContainer(context: Context) {
     private val api = HomeAssistantApi(wsClient)
     private val operationStore = OperationStore(appContext)
     private val revisionTracker = RevisionTracker()
+
+    val photoAnalysisService = PhotoAnalysisService(appContext, httpClient, authManager)
 
     val repository: NutritionRepository = DefaultNutritionRepository(
         ws = wsClient,
