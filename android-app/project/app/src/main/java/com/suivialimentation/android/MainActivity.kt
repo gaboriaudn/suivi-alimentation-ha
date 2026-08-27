718b83607e2b1d9b4b90abbf270c85db7406ac9f
gaboriaudn <155800237+gaboriaudn@users.noreply.github.com>
2026-08-27 18:27:59 +0200

    Corrige la navigation de la barre principale

--- android-app/project/app/src/main/java/com/suivialimentation/android/MainActivity.kt
+++ android-app/project/app/src/main/java/com/suivialimentation/android/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.browser.auth.AuthTabIntent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
